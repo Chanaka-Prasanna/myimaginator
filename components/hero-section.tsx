9d08eaef0afaa124b25460a9a21cb05aea1cf97a
Chanaka-Prasanna <chanakapinfo@gmail.com>
2024-12-21 22:14:36 +0530

    Completed and handled almost all bugs

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -25,12 +25,15 @@ const HeroBanner = () => {
           posts to stand out!
         </p>
         <div className="flex flex-row items-center justify-center space-x-4 md:justify-start">
-          <Button className="btn-gradient h-[40px] w-[120px] px-4 py-2 text-lg font-bold">
+          <Button
+            onClick={() => router.push("/caption-generator")}
+            className="btn-gradient h-[40px] w-[120px] px-4 py-2 text-lg font-bold"
+          >
             Try it Now
           </Button>
           <Button
             className="btn-gray h-[40px] w-[140px] px-4 py-2 text-lg font-bold"
-            onClick={() => router.push("/caption-generator")}
+            onClick={() => router.push("#pricing")}
           >
             View Pricing
           </Button>
