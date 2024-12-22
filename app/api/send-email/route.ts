c23b17bcf671d2e3cacaad66936c178f7ec438cc
Chanaka-Prasanna <chanakapinfo@gmail.com>
2024-12-22 09:33:04 +0530

    changed the navbar item colors,

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const apiKey = process.env.RESEND_API_KEY;
+const apiKey = process.env.RESEND_API_KEY || "";
 if (!apiKey) {
   throw new Error("RESEND_API_KEY is not defined in environment variables");
 }
